eba5ad3c55c57fd98eceed0ea114772d206a7c35
mzunanalfikri <zunanalfikri@gmail.com>
2020-07-09 19:41:22 +0700

    add db configurataion

--- src/main/java/com/zengine/techstore/TechstoreApplication.java
+++ src/main/java/com/zengine/techstore/TechstoreApplication.java
@@ -3,6 +3,7 @@ package com.zengine.techstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TechstoreApplication {
 
